57a0ffcc4dea328747b6f87d50e796cabb3f7112
Abdirahman <abdirahman.salah@student.moringaschool.com>
2025-03-18 14:31:36 +0300

    Complete JS math operators lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+
+// Variables provided:
+const num1 = 31;
+const num2 = 2;
+
+const num3 = 28;
+const num4 = 6;
+
+// 1. Multiply num1 and num2 so that the result is 62
+const multiply = num1 * num2; // 31 * 2 = 62
+
+// 2. Generate a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1; // random integer > 0
+
+// 3. Calculate remainder so that it equals 4
+const mod = num3 % num4; // 28 % 6 = 4
+
+// 4. Find the highest number in a set; should be 20
+const max = Math.max(1, 5, 10, 15, 20);
